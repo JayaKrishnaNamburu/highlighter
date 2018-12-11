ac566e5fbb73339faee57a2ef31201388709fb7c
JayaKrishna <namburu1995@gmail.com>
2018-12-11 16:56:29 +0530

    Fixed typos

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
-        library: 'simple-highlighter',
+        library: 'simple-text-highlighter',
         libraryTarget: 'umd'
     }
 }
