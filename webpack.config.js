1dcc22525d59ff395d473d13ec6c88ff42940d0a
JayaKrishna <namburu1995@gmail.com>
2019-03-13 14:23:01 +0530

    Made changes to env

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
-        library: 'simple-text-highlighter',
+        library: 'text-highlighter',
         libraryTarget: 'umd'
     }
 }
